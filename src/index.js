d1d72339c1c46ec2fa89c55dea53947cbad0f15b
Benjamin Biri <benjamin.biri.work@gmail.com>
2026-04-23 06:21:01 +0200

    Allow CORS for all

--- src/index.js
+++ src/index.js
@@ -13,13 +13,7 @@ const app = express();
 const PORT = Number(process.env.PORT || 5000);
 const CLIENT_URL = process.env.CLIENT_URL || "http://127.0.0.1:5173";
 
-app.use(cors({
-  origin(origin, callback) {
-    const allowed = [CLIENT_URL, "http://127.0.0.1:5173", "http://localhost:5173"];
-    if (!origin || allowed.includes(origin)) return callback(null, true);
-    return callback(new Error("Not allowed by CORS"));
-  }
-}));
+app.use(cors());
 app.use(express.json());
 
 async function ensureAdminUser() {
